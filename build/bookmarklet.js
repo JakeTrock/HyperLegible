1fb4d40f7a4283cf8da6867e0dbad508c4650c2b
Jacob Trock <jtbosshku@gmail.com>
2020-10-15 19:25:21 -0400

    init

---
+++ build/bookmarklet.js
@@ -0,0 +1,6 @@
+const cg = "@font-face {font-family: 'Atkinson';src: url('./Font/EOT/Atkinson-Hyperlegible-Regular-102.eof');src: url('./Font/EOT/Atkinson-Hyperlegible-Regular-102.eof?#iefix')format('embedded-opentype'), url('./Font/WOFF/Atkinson-Hyperlegible-Regular-102.woff') format('woff'), url('./Font/TTF/Atkinson-Hyperlegible-Regular-102.ttf')  format('truetype'), url('./Font/SVG/Atkinson-Hyperlegible-Regular-102.svg#svgFontName') format('svg');}";
+const ct = "font-family: 'Atkinson';";
+window.onload = function(){
+Array.prototype.slice.call(document.getElementsByTagName("head"))[0].appendChild(document.createElement('style')).innerHTML=cg;
+Array.prototype.slice.call(document.getElementsByTagName("*")).forEach(b=>b.style.cssText=ct)
+}
