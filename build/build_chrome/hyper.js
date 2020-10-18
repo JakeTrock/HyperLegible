30e70e686d0cb90d2008f119c9ee47a754044cb4
Jacob Trock <jtbosshku@gmail.com>
2020-10-18 17:46:04 -0400

    add domnode update listening to chrome

--- build/build_chrome/hyper.js
+++ build/build_chrome/hyper.js
@@ -1,7 +1,24 @@
+const cg = `@font-face {font-family: 'Atkinson';src: url('${chrome.runtime.getURL("Font/EOT/Atkinson-Hyperlegible-Regular-102.eof")}');src: url('${chrome.runtime.getURL("Font/EOT/Atkinson-Hyperlegible-Regular-102.eof")}?#iefix')format('embedded-opentype'), url('${chrome.runtime.getURL("Font/WOFF/Atkinson-Hyperlegible-Regular-102.woff")}') format('woff'), url('${chrome.runtime.getURL("Font/TTF/Atkinson-Hyperlegible-Regular-102.ttf")}')  format('truetype'), url('${chrome.runtime.getURL("Font/SVG/Atkinson-Hyperlegible-Regular-102.svg")}#Atkinson') format('svg');}`;
+const ct = 'Atkinson';
+const wl = ["P", "H1", "H2", "H3", "H4", "H5", "H6", "DIV", "BODY", "STRONG", "EM", "BLOCKQUOTE", "PRE", "CODE", "SPAN", "LI", "DD", "DT", "MARK", "INS", "DEL", "SUP", "SUB", "SMALL", "I", "B", "A", "TEXTAREA", "BUTTON"];
+const changeStyle = function (x) {
+    console.log(x);
+    x.filter(n => wl.includes(n.nodeName)).forEach(b => b.style.fontFamily = ct)
+}
+const changeElems = function (mutationList, observer) {
+    mutationList
+        .filter(n => mutationList.length != 0 && n.type == 'childList')
+        .forEach(b => changeStyle(Array.prototype.slice.call(b.addedNodes)));
+}
+const observerOptions = {
+    childList: true,
+    attributes: true,
+    subtree: true
+}
+const observer = new MutationObserver(changeElems);
 window.onload = function () {
-    const cg = `@font-face {font-family: 'Atkinson';src: url('${chrome.runtime.getURL("Font/EOT/Atkinson-Hyperlegible-Regular-102.eof")}');src: url('${chrome.runtime.getURL("Font/EOT/Atkinson-Hyperlegible-Regular-102.eof")}?#iefix')format('embedded-opentype'), url('${chrome.runtime.getURL("Font/WOFF/Atkinson-Hyperlegible-Regular-102.woff")}') format('woff'), url('${chrome.runtime.getURL("Font/TTF/Atkinson-Hyperlegible-Regular-102.ttf")}')  format('truetype'), url('${chrome.runtime.getURL("Font/SVG/Atkinson-Hyperlegible-Regular-102.svg")}#Atkinson') format('svg');}`;
-    const ct = 'Atkinson';
-    const wl = ["P", "H1", "H2", "H3", "H4", "H5", "H6", "DIV", "BODY", "STRONG", "EM", "BLOCKQUOTE", "PRE", "CODE", "SPAN", "LI", "DD", "DT", "MARK", "INS", "DEL", "SUP", "SUB", "SMALL", "I", "B", "A", "TEXTAREA", "BUTTON"];
+    const targetNode = document.querySelector("body");
+    observer.observe(targetNode, observerOptions);
     Array.prototype.slice.call(document.getElementsByTagName("head"))[0].appendChild(document.createElement('style')).innerHTML = cg;
-    Array.prototype.slice.call(document.getElementsByTagName("*")).filter(n => wl.includes(n.nodeName)).forEach(b => b.style.fontFamily = ct);
+    changeStyle(Array.prototype.slice.call(targetNode.getElementsByTagName("*")));
 }
