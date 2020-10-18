e8ec9bd6fbbcffb92e95e95aa8cc3ac860ceb485
Jacob Trock <jtbosshku@gmail.com>
2020-10-17 23:00:10 -0400

    no bg injects/added new support

--- build/background.js
+++
@@ -1,5 +0,0 @@
-chrome.tabs.onUpdated.addListener(function(tab){
-chrome.tabs.executeScript(tab.ib, {
-		file: 'hyper.js'
-});
-});
--- build/hyper.js
+++ build/hyper.js
@@ -1,8 +1,7 @@
 window.onload = function(){
 const cg = `@font-face {font-family: 'Atkinson';src: url('chrome-extension://__MSG_@@extension_id__/Font/EOT/Atkinson-Hyperlegible-Regular-102.eof');src: url('chrome-extension://__MSG_@@extension_id__/Font/EOT/Atkinson-Hyperlegible-Regular-102.eof?#iefix')format('embedded-opentype'), url('chrome-extension://__MSG_@@extension_id__/Font/WOFF/Atkinson-Hyperlegible-Regular-102.woff') format('woff'), url('chrome-extension://__MSG_@@extension_id__/Font/TTF/Atkinson-Hyperlegible-Regular-102.ttf')  format('truetype'), url('chrome-extension://__MSG_@@extension_id__/Font/SVG/Atkinson-Hyperlegible-Regular-102.svg#svgFontName') format('svg');}`;
-const ct = "font-family: 'Atkinson';";
-const wl = ["p","h1","h2","h3","h4","h5","h6","div","BODY","strong","em","blockquote","code","span","li","dd","dt","mark","ins","del","sup","sub","small","i","b","a"];
+const ct = 'Atkinson';
+const wl = ["P","H1","H2","H3","H4","H5","H6","DIV","BODY","STRONG","EM","BLOCKQUOTE","PRE","CODE","SPAN","LI","DD","DT","MARK","INS","DEL","SUP","SUB","SMALL","I","B","A","TEXTAREA","BUTTON"];
 Array.prototype.slice.call(document.getElementsByTagName("head"))[0].appendChild(document.createElement('style')).innerHTML=cg;
-Array.prototype.slice.call(document.getElementsByTagName("*")).filter(n => wl.includes(n.nodeName)).forEach(b=>b.style.cssText=ct);
-console.log("fff");
+Array.prototype.slice.call(document.getElementsByTagName("*")).filter(n => wl.includes(n.nodeName)).forEach(b=>b.style.fontFamily=ct);
 }
